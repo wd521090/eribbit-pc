1a03e3eb60e9b23d6014af4505bfc21eb08e77b4
mui1010 <541217150@qq.com>
2022-07-15 16:35:11 +0800

    完成首页底部布局

--- src/main.js
+++ src/main.js
@@ -9,9 +9,13 @@ import '@/assets/styles/common.less'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 /* import specific icons */
-import { faUser, faMobileScreen, faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+// import { faUser, faMobileScreen, faCartShopping, faHeadphonesSimple, faCircleQuestion } from '@fortawesome/free-solid-svg-icons'
 
 /* add icons to the library */
-library.add(faUser, faMobileScreen, faCartShopping)
+library.add(fas, fab)
+// library.add(faUser, faMobileScreen, faCartShopping, faHeadphonesSimple, faCircleQuestion)
 
 createApp(App).use(store).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,25 +1,29 @@
 <template>
   <AppTopNav></AppTopNav>
   <AppHeader></AppHeader>
-  <main>
+  <main class="app-body">
     <!-- 二级路由 -->
     <router-view></router-view>
   </main>
-  <footer>底部</footer>
+  <AppFooter>底部</AppFooter>
 </template>
 
 <script>
 import AppTopNav from '@/components/app-topnav'
 import AppHeader from '@/components/app-header'
+import AppFooter from '@/components/app-footer'
 export default {
   name: 'xtx-layout',
   components: {
     AppTopNav,
-    AppHeader
+    AppHeader,
+    AppFooter
   }
 }
 </script>
 
 <style scoped lang="less">
-
+.app-body {
+  min-height: 600px;
+}
 </style>
