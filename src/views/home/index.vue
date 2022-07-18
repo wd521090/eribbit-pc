223f850316f4b76301bc84e45db99a3428524b4d
mui1010 <541217150@qq.com>
2022-07-18 15:30:48 +0800

    实现首页主体内容-左侧分类
    大致步骤：
    准备左侧分类组件和基础布局
    从vuex中拿出9个分类数据，且值需要两个子分类，但是左侧是10个，需要补充一个品牌数据。
    使用计算属性完成上面逻辑
    渲染组件

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,16 +1,48 @@
 <template>
-  <div class="container">首页</div>
+  <div class="page-home">
+    <div class="home-entry">
+      <div class="container">
+        <!-- 左侧分类 -->
+        <HomeCategory></HomeCategory>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import HomeCategory from '@/views/home/components/home-category'
+
 export default {
-  name: 'HomePage'
+  name: 'HomePage',
+  components: {
+    HomeCategory
+  }
 }
 </script>
 
 <style scoped lang="less">
-.container {
-  .hoverShadow();
-  color: @xtxColor;
+.home-category {
+  width: 250px;
+  height: 500px;
+  background: rgba(0,0,0,0.8);
+  position: relative;
+  z-index: 99;
+  .menu {
+    li {
+      padding-left: 40px;
+      height: 50px;
+      line-height: 50px;
+      &:hover {
+        background: @xtxColor;
+      }
+      a {
+        margin-right: 4px;
+        color: #fff;
+        &:first-child {
+          font-size: 16px;
+        }
+      }
+    }
+  }
 }
 </style>
