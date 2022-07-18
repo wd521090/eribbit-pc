9ad128a9a4bb4e5598353f86e4ff53c3e9b096c0
mui1010 <541217150@qq.com>
2022-07-18 14:42:09 +0800

    完成吸顶头部，使用@vueuse/core工具库
    大致步骤：
    准备吸顶组件基础布局
    页面滚动到78px以上，显示吸顶组件
    useWindowScroll() 是@vueuse/core提供的api可返回当前页面滚动时候蜷曲的距离。x横向，y纵向

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,6 +1,7 @@
 <template>
   <AppTopNav></AppTopNav>
   <AppHeader></AppHeader>
+  <AppHeaderSticky></AppHeaderSticky>
   <main class="app-body">
     <!-- 二级路由 -->
     <router-view></router-view>
@@ -12,13 +13,16 @@
 import AppTopNav from '@/components/app-topnav'
 import AppHeader from '@/components/app-header'
 import AppFooter from '@/components/app-footer'
+import AppHeaderSticky from '@/components/app-header-sticky'
 import { useStore } from 'vuex'
+
 export default {
   name: 'xtx-layout',
   components: {
     AppTopNav,
     AppHeader,
-    AppFooter
+    AppFooter,
+    AppHeaderSticky
   },
   setup () {
     const store = useStore()
