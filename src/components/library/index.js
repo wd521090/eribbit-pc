deb3e5c17f4378748b1d061e179496b14c9e2e8e
mui1010 <541217150@qq.com>
2022-07-19 16:01:48 +0800

    目的： 提取首页的公用面板进行复用
    大致思路：
    
    头部
    标题和副标题由props传入
    右侧内容由插槽传入
    查看更多使用次数多封装成全局组件
    主体
    全部由插槽传入

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -1,5 +1,6 @@
 import XtxSkeleton from '@/components/library/xtx-skeleton'
 import XtxCarousel from '@/components/library/xtx-carousel'
+import XtxMore from '@/components/library/xtx-more'
 
 export default {
   install (app) {
@@ -7,5 +8,6 @@ export default {
     // 如果要挂载原型 app.config.globalProperties 方式
     app.component(XtxSkeleton.name, XtxSkeleton)
     app.component(XtxCarousel.name, XtxCarousel)
+    app.component(XtxMore.name, XtxMore)
   }
 }
