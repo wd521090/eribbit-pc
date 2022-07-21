1097fd8c2faebcaa5da4893a9aad1397eb49dc74
mui1010 <541217150@qq.com>
2022-07-21 10:25:54 +0800

    目的： 当图片进入可视区域内去加载图片，且处理加载失败，封装成指令。

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -1,6 +1,7 @@
 import XtxSkeleton from '@/components/library/xtx-skeleton'
 import XtxCarousel from '@/components/library/xtx-carousel'
 import XtxMore from '@/components/library/xtx-more'
+import defaultImg from '@/assets/images/200.png'
 
 export default {
   install (app) {
@@ -9,5 +10,29 @@ export default {
     app.component(XtxSkeleton.name, XtxSkeleton)
     app.component(XtxCarousel.name, XtxCarousel)
     app.component(XtxMore.name, XtxMore)
+    defineDirective(app)
   }
+}
+
+const defineDirective = (app) => {
+  // 图片懒加载指令
+  app.directive('lazy', {
+    mounted (el, binding) {
+      const observer = new IntersectionObserver(([{ isIntersecting }]) => {
+        if (isIntersecting) {
+          // 停止观察
+          observer.unobserve(el)
+          el.onerror = () => {
+            // 加载失败，设置默认图
+            el.src = defaultImg
+          }
+          el.src = binding.value
+        }
+      }, {
+        threshold: 0.01
+      })
+      // 开启观察
+      observer.observe(el)
+    }
+  })
 }
--- src/views/home/components/home-new.vue
+++ src/views/home/components/home-new.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="home-new">
     <HomePanel title="新鲜好物" sub-title="新鲜出炉 品质靠谱">
-      <template #right><XtxMore path="/" /></template>
+      <template #right>
+        <XtxMore path="/"/>
+      </template>
       <div ref="target" style="position: relative;height: 406px;">
         <Transition name="fade">
-      <!-- 面板内容 -->
-      <ul v-if="goods.length" class="goods-list">
-        <li v-for="item in goods" :key="item.id">
-          <RouterLink :to="`/product/${item.id}`">
-            <img :src="item.picture" alt="">
-            <p class="name ellipsis">{{item.name}}</p>
-            <p class="price">&yen;{{item.price}}</p>
-          </RouterLink>
-        </li>
-      </ul>
+          <!-- 面板内容 -->
+          <ul v-if="goods.length" class="goods-list">
+            <li v-for="item in goods" :key="item.id">
+              <RouterLink :to="`/product/${item.id}`">
+                <img :src="item.picture" alt="">
+                <p class="name ellipsis">{{ item.name }}</p>
+                <p class="price">&yen;{{ item.price }}</p>
+              </RouterLink>
+            </li>
+          </ul>
           <HomeSkeleton bg="#f0f9f4" v-else></HomeSkeleton>
         </Transition>
       </div>
@@ -34,8 +36,14 @@ export default {
     HomeSkeleton
   },
   setup () {
-    const { target, result } = useLazyData(findNew)
-    return { goods: result, target }
+    const {
+      target,
+      result
+    } = useLazyData(findNew)
+    return {
+      goods: result,
+      target
+    }
   }
 }
 </script>
@@ -45,20 +53,24 @@ export default {
   display: flex;
   justify-content: space-between;
   height: 406px;
+
   li {
     width: 306px;
     height: 406px;
     background: #f0f9f4;
     .hoverShadow();
+
     img {
       width: 306px;
       height: 306px;
     }
+
     p {
       font-size: 22px;
       padding: 12px 30px 0 30px;
       text-align: center;
     }
+
     .price {
       color: @priceColor;
     }
