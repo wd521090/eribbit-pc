38dbb41c882e917955caf23549ea310c721f2c9f
mui1010 <541217150@qq.com>
2022-07-21 14:43:48 +0800

    目的： 展示各个子级分类下推荐的商品基础布局
    大致步骤：
    
    准备单个商品组件
    完成推荐商品区块布局

--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -1,15 +1,139 @@
 <template>
-  <!-- 面包屑 -->
-  <div></div>
+  <div class="top-category">
+    <div class="container">
+      <!-- 面包屑 -->
+      <div class="xtx-bread">
+        <el-breadcrumb :separator-icon="ArrowRight">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>{{ topCategory.name }}</el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+      <!-- 轮播图 -->
+      <XtxCarousel :sliders="sliders" style="height:500px" />
+      <!-- 所有二级分类 -->
+      <div class="sub-list">
+        <h3>全部分类</h3>
+        <ul>
+          <li v-for="item in topCategory.children" :key="item.id">
+            <a href="javascript:">
+              <img :src="item.picture" >
+              <p>{{ item.name }}</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <!-- 不同分类商品 -->
+      <div class="ref-goods">
+        <div class="head">
+          <h3>- 海鲜 -</h3>
+          <p class="tag">温暖柔软，品质之选</p>
+          <XtxMore />
+        </div>
+        <div class="body">
+          <GoodsItem v-for="i in 5" :key="i" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 
+import XtxCarousel from '@/components/library/xtx-carousel'
+import { computed, ref } from 'vue'
+import { findBanner } from '@/api/home'
+import { ArrowRight } from '@element-plus/icons-vue'
+import { useStore } from 'vuex'
+import { useRoute } from 'vue-router'
+import GoodsItem from '@/views/category/components/goods-item'
+
 export default {
-  name: 'TopCategory'
+  name: 'TopCategory',
+  components: {
+    XtxCarousel,
+    GoodsItem
+  },
+  setup() {
+    const sliders = ref([])
+    findBanner().then(data => {
+      sliders.value = data.result
+    })
+    const store = useStore()
+    const route = useRoute()
+    const topCategory = computed(() => {
+      let cate = {}
+      const item = store.state.category.list.find(item => {
+        return item.id === route.params.id
+      })
+      if (item) cate = item
+      return cate
+    })
+    return { sliders, ArrowRight, topCategory }
+  }
 }
 </script>
 
 <style scoped lang="less">
-
+.top-category {
+  h3 {
+    font-size: 28px;
+    color: #666;
+    font-weight: normal;
+    text-align: center;
+    line-height: 100px;
+  }
+  .sub-list {
+    margin-top: 20px;
+    background-color: #fff;
+    ul {
+      display: flex;
+      padding: 0 32px;
+      flex-wrap: wrap;
+      li {
+        width: 168px;
+        height: 160px;
+        a {
+          text-align: center;
+          display: block;
+          font-size: 16px;
+          img {
+            width: 100px;
+            height: 100px;
+          }
+          p {
+            line-height: 40px;
+          }
+          &:hover {
+            color: @xtxColor;
+          }
+        }
+      }
+    }
+  }
+  .ref-goods {
+    background-color: #fff;
+    margin-top: 20px;
+    position: relative;
+    .head {
+      .xtx-more {
+        position: absolute;
+        top: 20px;
+        right: 20px;
+      }
+      .tag {
+        text-align: center;
+        color: #999;
+        font-size: 20px;
+        position: relative;
+        top: -20px;
+      }
+    }
+    .body {
+      display: flex;
+      justify-content: flex-start;
+      flex-wrap: wrap;
+      padding: 0 65px 30px;
+    }
+  }
+}
 </style>
