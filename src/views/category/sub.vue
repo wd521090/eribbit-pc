9c6fe466232ed7d462f71d70a892b2ef1cb54b77
mui1010 <541217150@qq.com>
2022-07-21 15:50:52 +0800

    目的：根据二级类目ID展示多级面包屑
    大致思路：
    
    封装一个独立的组件来完成，因为需要加动画。
    
    使用组合API的方式通过计算属性得到所需数据

--- src/views/category/sub.vue
+++ src/views/category/sub.vue
@@ -1,10 +1,20 @@
 <template>
-  <div>SubCategory</div>
+  <div class='sub-category'>
+    <div class="container">
+      <!-- 面包屑 -->
+      <SubBread />
+    </div>
+  </div>
 </template>
 
 <script>
+import SubBread from '@/views/category/components/sub-bread'
+
 export default {
-  name: 'SubCategory'
+  name: 'SubCategory',
+  components: {
+    SubBread
+  }
 }
 </script>
 
