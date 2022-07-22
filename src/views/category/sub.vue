6da73c52dd714b4ab3f542132d79f479a23ccf4a
mui1010 <541217150@qq.com>
2022-07-22 10:17:07 +0800

    目的：封装排序组件，完成排序切换效果
    大致步骤：
    
    定义一个组件 sub-sort，完成基础布局
    在 sub.vue 组件使用
    完成切换排序时候的交换效果

--- src/views/category/sub.vue
+++ src/views/category/sub.vue
@@ -2,8 +2,14 @@
   <div class='sub-category'>
     <div class="container">
       <!-- 面包屑 -->
-      <SubBread />
+      <SubBread/>
       <SubFilter/>
+      <!-- 结果区域 -->
+      <div class="goods-list">
+        <!-- 排序 -->
+        <SubSort/>
+        <!-- 列表 -->
+      </div>
     </div>
   </div>
 </template>
@@ -11,16 +17,22 @@
 <script>
 import SubBread from '@/views/category/components/sub-bread'
 import SubFilter from '@/views/category/components/sub-filter'
+import SubSort from '@/views/category/components/sub-sort'
 
 export default {
   name: 'SubCategory',
   components: {
     SubBread,
-    SubFilter
+    SubFilter,
+    SubSort
   }
 }
 </script>
 
 <style scoped lang="less">
-
+.goods-list {
+  background: #fff;
+  padding: 0 25px;
+  margin-top: 25px;
+}
 </style>
