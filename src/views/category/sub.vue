240fc47ae82087ab34d043cac9f8fe2a571db10a
mui1010 <541217150@qq.com>
2022-07-22 11:07:24 +0800

    目的：实现结果区域商品展示。
    大致步骤：
    
    完成结果区域商品布局
    完成 xtx-infinite-loading 组件封装
    使用 xtx-infinite-loading 完成数据加载和渲染

--- src/views/category/sub.vue
+++ src/views/category/sub.vue
@@ -9,6 +9,12 @@
         <!-- 排序 -->
         <SubSort/>
         <!-- 列表 -->
+        <ul>
+          <li v-for="goods in goodsList" :key="goods.id" >
+            <GoodsItem :goods="goods" />
+          </li>
+        </ul>
+        <XtxInfiniteLoading :loading="loading" :finished="finished" @infinite="getData" />
       </div>
     </div>
   </div>
@@ -18,13 +24,64 @@
 import SubBread from '@/views/category/components/sub-bread'
 import SubFilter from '@/views/category/components/sub-filter'
 import SubSort from '@/views/category/components/sub-sort'
+import GoodsItem from '@/views/category/components/goods-item'
+import { ref, watch } from 'vue'
+import { findSubCategoryGoods } from '@/api/category'
+import { useRoute } from 'vue-router'
 
 export default {
   name: 'SubCategory',
   components: {
     SubBread,
     SubFilter,
-    SubSort
+    SubSort,
+    GoodsItem
+  },
+  setup () {
+    // 1. 基础布局
+    // 2. 无限加载组件
+    // 3. 动态加载数据且渲染
+    // 4. 任何筛选条件变化需要更新列表
+    const route = useRoute()
+
+    const loading = ref(false)
+    const finished = ref(false)
+    const goodsList = ref([])
+    // 查询参数
+    let reqParams = {
+      page: 1,
+      pageSize: 20
+    }
+    // 获取数据函数
+    const getData = () => {
+      loading.value = true
+      reqParams.categoryId = route.params.id
+      findSubCategoryGoods(reqParams).then(({ result }) => {
+        if (result.items.length) {
+          goodsList.value.push(...result.items)
+          reqParams.page++
+        } else {
+          // 加载完毕
+          finished.value = true
+        }
+        // 请求结束
+        loading.value = false
+      })
+    }
+
+    // 切换二级分类重新加载
+    watch(() => route.params.id, (newVal) => {
+      if (newVal && route.path === ('/category/sub/' + newVal)) {
+        goodsList.value = []
+        reqParams = {
+          page: 1,
+          pageSize: 20
+        }
+        finished.value = false
+      }
+    })
+
+    return { loading, finished, goodsList, getData }
   }
 }
 </script>
@@ -34,5 +91,17 @@ export default {
   background: #fff;
   padding: 0 25px;
   margin-top: 25px;
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 0 5px;
+    li {
+      margin-right: 20px;
+      margin-bottom: 20px;
+      &:nth-child(5n) {
+        margin-right: 0;
+      }
+    }
+  }
 }
 </style>
